9f923d9719b90b1687e8df0fc541e689cc989d28
Israel Robotnick <israel.robotnick@gmail.com>
2021-08-04 16:09:30 -0700

    include frameId and synced with server

--- colab.py
+++ colab.py
@@ -43,11 +43,7 @@ def checkMyAddress():
         checkMyAddress()
     else:
         # Website is up
-        ngrokURI = !curl - -silent - -show - error
-        http: // 127.0
-        .0
-        .1: 4040 / api / tunnels | sed - nE
-        's/.*public_url":"https:..([^"]*).*/\1/p'
+        ngrokURI = !curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'
         ngrokURI = """http://{}""".format(ngrokURI[0])
         print(ngrokURI)
 
